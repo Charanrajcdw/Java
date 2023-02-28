fb7546827ea24741ab8b25abfed287a1df53a49f
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-03-01 00:14:58 +0530

    Modified factory pattern

--- Exercise 4/creationalPattern/FactoryPattern.java
+++ Exercise 4/creationalPattern/FactoryPattern.java
@@ -1,37 +1,77 @@
 package creationalPattern;
 
-abstract class Laptop{
+abstract class Laptop {
 	public abstract void work();
 }
 
-class LenovoLaptop extends Laptop{
+class LenovoLaptop extends Laptop {
 	@Override
 	public void work() {
-		System.out.println("Lenovo laptop made...");
+		System.out.println("Working in Lenovo laptop...");
 	}
 }
 
-class DellLaptop extends Laptop{
+class DellLaptop extends Laptop {
 	@Override
 	public void work() {
-		System.out.println("Dell laptop made...");
+		System.out.println("Working in Dell laptop...");
 	}
 }
 
-class LaptopFactory {
-	public static Laptop getLaptop(String name) {
-		if(name.equals("Lenovo"))
-			return new LenovoLaptop();
-		else if(name.equals("Dell"))
-			return new DellLaptop();
-		else
-			return null;
+abstract class LaptopManufacturer {
+	public abstract Laptop makeLaptop();
+}
+
+class LenovoManufacturer extends LaptopManufacturer {
+	public LenovoManufacturer() {
+	}
+
+	@Override
+	public Laptop makeLaptop() {
+		return new LenovoLaptop();
+	}
+}
+
+class DellManufacturer extends LaptopManufacturer {
+	public DellManufacturer() {
+	}
+
+	@Override
+	public Laptop makeLaptop() {
+		return new DellLaptop();
+	}
+}
+
+class Showroom {
+	LaptopManufacturer laptopManufacturer;
+
+	public LaptopManufacturer getLaptopManufacturer() {
+		return laptopManufacturer;
+	}
+
+	public void setLaptopManufacturer(String name) {
+		try {
+			this.laptopManufacturer = (LaptopManufacturer) Class.forName("creationalPattern." + name + "Manufacturer")
+					.getConstructor().newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Laptop getLaptop() {
+		return getLaptopManufacturer().makeLaptop();
 	}
 }
 
 public class FactoryPattern {
 	public static void main(String[] args) {
-		Laptop myLaptop=LaptopFactory.getLaptop("Dell");
-		myLaptop.work();
+		Showroom showroomA = new Showroom();
+		showroomA.setLaptopManufacturer("Dell");
+
+		Showroom showroomB = new Showroom();
+		showroomB.setLaptopManufacturer("Lenovo");
+
+		Laptop laptop = showroomB.getLaptop();
+		laptop.work();
 	}
 }
