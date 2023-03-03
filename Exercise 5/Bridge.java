a8d5321f8bedf0736b9a5ae5323e496f9fb32530
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-03-03 11:05:18 +0530

    Exercise 5 completed

---
+++ Exercise 5/Bridge.java
@@ -0,0 +1,60 @@
+package patterns;
+
+abstract class Shape {
+	Color color;
+
+	public Shape(Color color) {
+		this.color = color;
+	}
+
+	public abstract void draw();
+}
+
+class Square extends Shape {
+	public Square(Color color) {
+		super(color);
+	}
+
+	@Override
+	public void draw() {
+		System.out.println("A " + color.getColor() + " square...");
+	}
+}
+
+class Circle extends Shape {
+	public Circle(Color color) {
+		super(color);
+	}
+
+	@Override
+	public void draw() {
+		System.out.println("A " + color.getColor() + " circle...");
+	}
+}
+
+interface Color {
+	public String getColor();
+}
+
+class Red implements Color{
+	@Override
+	public String getColor() {
+		return "red color";
+	}
+}
+
+class Blue implements Color{
+	@Override
+	public String getColor() {
+		return "blue color";
+	}
+}
+
+public class Bridge {
+	public static void main(String[] args) {
+		Shape redSqaure=new Square(new Red());
+		Shape blueCircle=new Circle(new Blue());
+		redSqaure.draw();
+		blueCircle.draw();
+	}
+}
---
+++ Exercise 5/Flyweight.java
@@ -0,0 +1,92 @@
+package patterns;
+
+import java.util.HashMap;
+import java.util.Random;
+
+interface Player {
+	public void setWeapon(String weapon);
+
+	public void mission();
+}
+
+class Terrorist implements Player {
+	private String task, weapon;
+
+	public Terrorist() {
+		System.out.println("Terrorist player object has been created...");
+		task = "capture hostages";
+	}
+
+	@Override
+	public void setWeapon(String weapon) {
+		this.weapon = weapon;
+	}
+
+	@Override
+	public void mission() {
+		System.out.println("Terrorist [task=" + task + ", weapon=" + weapon + "]");
+	}
+}
+
+class CounterTerrorist implements Player {
+	private String task, weapon;
+
+	public CounterTerrorist() {
+		System.out.println("CounterTerrorist player object has been created...");
+		task = "rescue hostages";
+	}
+
+	@Override
+	public void setWeapon(String weapon) {
+		this.weapon = weapon;
+	}
+
+	@Override
+	public void mission() {
+		System.out.println("CounterTerrorist [task=" + task + ", weapon=" + weapon + "]");
+	}
+}
+
+class PlayerFactory {
+	private static HashMap<String, Player> map = new HashMap<>();
+
+	public static Player getPlayer(String type) {
+		Player player = null;
+		if (map.containsKey(type))
+			player = map.get(type);
+		else if (type == "Terrorist") {
+			player = new Terrorist();
+			map.put(type, player);
+		} else if (type == "CounterTerrorist") {
+			player = new CounterTerrorist();
+			map.put(type, player);
+		}
+		return player;
+	}
+}
+
+public class Flyweight {
+	private static String[] playerType = { "Terrorist", "CounterTerrorist" };
+	private static String[] weapons = { "AK-47", "M134", "M16A4", "MK-22" };
+
+	public static String getRandPlayerType() {
+		Random random = new Random();
+		int randomInt = random.nextInt(playerType.length);
+		return playerType[randomInt];
+	}
+
+	public static String getRandWeapon() {
+		Random random = new Random();
+		int randomInt = random.nextInt(weapons.length);
+		return weapons[randomInt];
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			Player player = PlayerFactory.getPlayer(getRandPlayerType());
+			player.setWeapon(getRandWeapon());
+			player.mission();
+		}
+
+	}
+}
